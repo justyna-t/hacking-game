7a111171291485297eca33f3f0ec2256f4f1bd56
justyna-t <justyna.anita.kaminska@gmail.com>
2018-07-18 10:16:24 +0200

    Ended the first step "display header" of functional test plan

--- hacking1.py
+++ hacking1.py
@@ -4,6 +4,8 @@
 # The game indicates that the player failed to guess the password correctly.
 
 # display header
+print("DEBUG MODE")
+print("1 ATTEMPT(S) LEFT\n")
 # display password
 # prompt for guess
 # end game
