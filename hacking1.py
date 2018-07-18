8f201dd4e82f146838ea50d066d3a7c33782f699
justyna-t <justyna.anita.kaminska@gmail.com>
2018-07-18 10:13:02 +0200

    This is first version of hacking game

---
+++ hacking1.py
@@ -0,0 +1,11 @@
+# Hacking Version 1
+# This is a text-based password guessing game that displays a list of potential
+# computer passwords. The player is allowed 1 attempt to guess the password.
+# The game indicates that the player failed to guess the password correctly.
+
+# display header
+# display password
+# prompt for guess
+# end game
+#   display faulure outcom
+#   prompt for end
