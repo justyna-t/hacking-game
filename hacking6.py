756f8395e22d65da092ff98dbc0d0012c4db0b32
justyna-t <justyna.anita.kaminska@gmail.com>
2018-08-16 14:09:09 +0200

    Created function which is creating window

--- hacking6.py
+++ hacking6.py
@@ -12,104 +12,15 @@
 from uagame import Window
 from time import sleep
 
-# create window
-window = Window("Hacking", 600, 500)
-window.set_font_name("couriernew")
-window.set_font_size(18)
-window.set_font_color("green")
-window.set_bg_color("black")
+def main():
+    window = create_window()
 
-# get window height and width
-window_height = window.get_height()
-window_width = window.get_width()
+def create_window():
+    window = Window("Hacking", 600, 500)
+    window.set_font_name("couriernew")
+    window.set_font_size(18)
+    window.set_font_color("green")
+    window.set_bg_color("black")
+    return window
 
-# display header
-line_x = 0
-line_y = 0
-string_high = window.get_font_height()
-#    for header line in header
-attempts = 4
-header = ["DEBUG MODE", "%d ATTEMPT(S) LEFT" % attempts, ""]
-for header_line in header:
-    window.draw_string(header_line, line_x, line_y)
-    window.update()
-    line_y += string_high
-    sleep(0.3)
-
-# display password
-#   for password in password list
-password_list = ["PROVIDE", "SETTING", "CANTINA", "CUTTING", "HUNTERS",
-                 "SURVIVE", "HEARING", "HUNTING", "REALIZE", "NOTHING",
-                 "OVERLAP", "FINDING", "PUTTING", ""]
-for password in password_list:
-    window.draw_string(password, line_x, line_y)
-    window.update()
-    sleep(0.3)
-    line_y += string_high
-
-# choose password
-password = password_list[7]
-
-# get guesses
-#   prompt for guess
-guess = window.input_string("ENTER PASSWORD >", line_x, line_y)
-line_y += string_high
-attempts -= 1
-
-#   get guess until it is not equal password and attempts left greater than 0
-while guess != password and attempts > 0 :
-    window.draw_string(str(attempts), 0, string_high)
-
-#       check warning
-    if attempts == 1:
-        message = "*** LOCKOUT WARNING ***"
-        x = window_width - window.get_string_width(message)
-        y = window_height - string_high
-        window.draw_string(message, x, y)
-
-#       prompt for guess
-    guess = window.input_string("ENTER PASSWORD >", line_x, line_y)
-    line_y += string_high
-    attempts -= 1
-
-# end game
-#   clear window
-window.clear()
-
-#   display outcome
-#       for outcome in outcome list
-#           compute y coordinate for every line
-line_y = window_height - 7 * string_high
-line_y //= 2
-outcome_list = [guess, ""]
-if guess == password:
-    successful_outcome = ["EXITING DEBUG MODE", "", "LOGIN SUCCESSFUL - WELCOME\
- BACK", ""]
-    outcome_list += successful_outcome
-    prompt = "PRESS ENTER TO CONTINUE"
-
-else:
-    failure_outcome = ["LOGIN FAILURE - TERMINAL LOCKED", "", "PLEASE CONTACT \
-AN ADMINISTRATOR", ""]
-    outcome_list += failure_outcome
-    prompt = "PRESS ENTER TO EXIT"
-
-for outcome in outcome_list:
-#           compute x coordinate
-    line_x = window_width - window.get_string_width(outcome)
-    line_x //= 2
-
-    window.draw_string(outcome, line_x, line_y)
-    window.update()
-    sleep(0.3)
-    line_y += string_high
-
-#   prompt for end
-#           compute x coordinate
-line_x = window_width - window.get_string_width(prompt)
-line_x //= 2
-
-window.input_string(prompt, line_x, line_y)
-
-#   close window
-window.close()
+main()
